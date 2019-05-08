e142665f090ac0a6a76a2c4fdd45147bb469079d
Alexander Sysoev <alexsblsss@gmail.com>
2019-05-08 17:15:40 -0400

    Remove secret file dependecy

--- config/test.exs
+++ config/test.exs
@@ -4,5 +4,7 @@ config :ex_queue_bus_client,
   do_not_start_tree: true,
   event_handler: ExQueueBusClient.EventHandlerMock
 
+config :ex_aws,
+  access_key_id:      [System.get_env("AWS_ACCESS_KEY"), :instance_role],
+  secret_access_key:  [System.get_env("AWS_SECRET_KEY"), :instance_role]
 
-import_config "test.secret.exs"
