1dc84c19c48fd83797ff2a494f5a8a7b61f87759
Alexander Sysoev <alexsblsss@gmail.com>
2019-05-15 11:15:58 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExQueueBusClient.Mixfile do
   def project do
     [
       app: :ex_queue_bus_client,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
