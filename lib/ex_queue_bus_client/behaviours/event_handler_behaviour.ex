e536e5687033355d28794fdf1937dc3c00edb0fb
Alexander Sysoev <alexsblsss@gmail.com>
2019-07-01 14:51:38 -0400

    Change handle_event function spec

--- lib/ex_queue_bus_client/behaviours/event_handler_behaviour.ex
+++ lib/ex_queue_bus_client/behaviours/event_handler_behaviour.ex
@@ -1,6 +1,4 @@
 defmodule ExQueueBusClient.EventHandlerBehaviour do
-  @type event :: String.t()
-  @type provider :: :letstalk | :nuntium | :hangout
-
-  @callback handle_event(event, provider, map()) :: :process | :skip
+  @callback handle_event(event :: binary, provider :: binary, payload :: map()) ::
+              :process | :skip
 end
