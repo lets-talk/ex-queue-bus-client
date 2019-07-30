a7f5d0a29cff309ecad4b2649a9c8f2e9a4aa403
Alexander Sysoev <alexsblsss@gmail.com>
2019-07-30 18:08:36 -0400

    Add bus supervision test

---
+++ test/bus_test.exs
@@ -0,0 +1,43 @@
+defmodule ExQueueBusClient.BusTest do
+  use ExUnit.Case, async: false
+
+  defmodule DummyBus do
+    use ExQueueBusClient.Bus,
+      otp_app: :ex_queue_bus_client,
+      event_handler: ExQueueBusClient.EventHandler,
+      send_via: :sns,
+      receive_with: :sqs
+  end
+
+  @settings [consumers_count: 3, sns_topic_arn: "test-topic", sqs_queue_name: "test-queue"]
+
+  setup do
+    Application.put_env(:ex_queue_bus_client, ExQueueBusClient.BusTest.DummyBus, @settings)
+    :ok
+  end
+
+  describe "module that uses Bus" do
+    test "it has a start_link/1 function exported" do
+      assert function_exported?(DummyBus, :start_link, 1)
+    end
+
+    test "it starts bus supervisor with children and correct config" do
+      assert {:ok, pid} = DummyBus.start_link()
+
+      assert [
+               {"Consumer-3", _, _, _},
+               {"Consumer-2", _, _, _},
+               {"Consumer-1", _, _, _},
+               {ExQueueBusClient.SQS.Producer, _, _, _},
+               {ExQueueBusClient.BusSupervisor.Config, _, _, _}
+             ] = Supervisor.which_children(pid)
+
+      config =
+        @settings
+        |> Keyword.put(:event_handler, ExQueueBusClient.EventHandler)
+        |> Keyword.put(:send_via, :sns)
+
+      assert ^config = ExQueueBusClient.BusSupervisor.Config.config()
+    end
+  end
+end
