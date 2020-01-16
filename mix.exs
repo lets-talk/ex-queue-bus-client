4d9f7e1467e98ff6350557a9b14ad2ee6438a6ef
Alexander Sysoev <alexsblsss@gmail.com>
2020-01-16 09:40:17 -0300

    Upgrade hackney

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule ExQueueBusClient.Mixfile do
       {:ex_aws, "~> 2.1"},
       {:ex_aws_sqs, "~> 2.0"},
       {:ex_aws_sns, "~> 2.0"},
-      {:hackney, "~> 1.9"},
+      {:hackney, "~> 1.15 and >= 1.15.2"},
       {:poison, "~> 3.0"},
       {:sweet_xml, "~> 0.6"},
       {:gen_stage, "~> 0.14"},
